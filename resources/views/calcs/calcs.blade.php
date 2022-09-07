0aeb714b456c5e8201bc69ccee1dc3993c3e8088
tetuyakawarada <tetuya.kawarada@gmail.com>
2022-09-07 11:09:37 +0900

    view実装しました

---
+++ resources/views/calcs/calcs.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>計算アプリ</title>
+</head>
+<body>
+    <h1>計算アプリ</h1>
+</body>
+</html>
+
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return '<h1>welcome</h1>';
+});
+
+Route::get('/calcs', function () {
+    return view('calcs.calcs');
 });
